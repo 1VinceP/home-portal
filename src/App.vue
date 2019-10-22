180927c48287042544bdac790898899560e40bce
Vincent Palmer <vpalmergraphics@gmail.com>
2019-10-22 17:50:10 -0400

    homepage layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
+<script>
+import HouseIcon from 'vue-material-design-icons/Home.vue';
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+
+export default {
+  data: () => ({
+    navOpen: false,
+  }),
+
+  methods: {
+    handleNav() {
+      this.navOpen = !this.navOpen;
+    },
+  },
+
+  components: { HouseIcon, MenuIcon },
+};
+</script>
+
 <template>
   <div id="app">
-    <div class="nav">
-      <router-link to="/">Home</router-link>
+    <div :class="['nav', { 'nav-large': navOpen }]">
+      <router-link to="/"><HouseIcon /></router-link>
+      <!-- <button @click="handleNav()">{{ navOpen ? 'Close' : 'Open' }}</button> -->
+      <MenuIcon class="menu" @click="handleNav()" />
     </div>
     <div class="page">
       <router-view/>
@@ -10,47 +31,58 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
 /* variables */
-// sizes
-$navSize: 70px;
-// colors
-$orange: #ff7d00;
-$red: #78290f;
-$blue: #1e96fc;
-$navy: #001524;
-// formats
-$shadow: 0 0 6px 1px #999;
+:root {
+  --navSize: 70px;
+  // colors
+  --orange: #ff7d00;
+  --red: #78290f;
+  --blue: #1e96fc;
+  --navy: #001524;
+  --grey: #999999;
+  // formats
+  --shadow: 0 0 6px 1px var(--grey);
+}
 
 #app {
   display: flex;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  & * { font-family: 'Open Sans', sans-serif; }
 }
 
 .nav {
   height: 100vh;
-  width: $navSize;
+  width: var(--navSize);
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 30px 0;
   position: fixed;
-  // box-shadow: 0 0 6px 1px $navy;
-  box-shadow: $shadow;
+  box-shadow: var(--shadow);
+  transition: all .2s ease-in-out;
+  &.nav-large {
+    width: 300px;
+  }
   & a {
     font-weight: bold;
     color: #2c3e50;
   }
   & a.router-link-exact-active {
-    color: $blue;
+    color: var(--blue);
+  }
+  & .menu {
+    cursor: pointer;
   }
 }
 
 .page {
   min-height: 100vh;
   width: 100%;
+  padding: 16px;
+  padding-left: calc(var(--navSize) + 16px);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    search: '',
+    // sIndex: 1,
+    // sItems: ['Rewards', 'Users', 'Tasks', 'Meals'],
+    sDisplay: '',
+    // searchRotation: true,
+  }),
+
+  mounted() {
+    // setInterval(() => {
+    //   this.sDisplay = this.sItems[this.sIndex];
+    //   this.sIndex = this.sIndex === this.sItems.length - 1 ? 0 : this.sIndex + 1;
+    // }, 8000);
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <div class="search-container">
+      <!-- <input class="search" :placeholder="`Find ${this.sDisplay}`" :value="search" /> -->
+      <input class="search" :value="search" />
+      <span class="search-button">Search</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.search-container {
+  height: 40px;
+  width: 500px;
+  align-self: flex-end;
+  position: relative;
+  & .search {
+    height: 40px;
+    width: 500px;
+    border: 1px solid var(--grey);
+    border-radius: 50px;
+    padding: 0 80px 0 16px ;
+    font-size: 16px;
+    outline: none;
+    transition: all .15s ease-in-out;
+    &:focus {
+      border: 2px solid var(--orange);
+    }
+  }
+  & .search-button {
+    position: absolute;
+    right: 12px;
+    top: 7px;
+    border: 1px solid transparent;
+    color: var(--blue);
+    font-size: 18px;
+    padding: 3px;
+    cursor: pointer;
+    transition: all .03s ease-in-out;
+    &:active {
+      border: 1px solid var(--blue);
+      border-radius: 3px;
+    }
+  }
+}
+
+</style>
